7299d3304fbf7afae1e6f8417573217c9ec0e68b
Shoxrux Yuldashov <deagleshox@mail.ru>
2025-02-16 06:56:44 +0000

    Исправление

--- word_detect.py
+++ word_detect.py
@@ -1,10 +1,5 @@
-import string
-
-LATIN_GLAS = set('AEIOUY')
-CYRILIC_GLAS = set('АЕЁИОУЫЭЮЯ')
-LATIN_UPPER_LETTERS = set(string.ascii_uppercase)
-CYRILLIC_UPPER_LETTERS = set('абвгдеёжзийклмнопрстуфхцчшщъыьэюя'.upper())
-
+ENG_VOWELS: str = 'AEIOUY'
+CYRILLIC_VOWELS: str = 'АЕЁИОУЫЭЮЯ'
 
 print('Добро пожаловать в программу "Буква-Детектив!\n')
 
@@ -12,44 +7,48 @@ print('Выберите алфавит:\n')
 print('1. Латинский')
 print('2. Кириллица\n')
 
+
 def is_correct_alphabet_number(n: int) -> bool:
-    if n not in [1,2]:
+    if n not in [1, 2]:
         print("Упс! Выбран неверный режим. Попробуйте ещё раз...")
         return False
     return True
-    
 
-def alphabet_n_letter_of_alphabet(num: int, el: str) -> bool:
-    if num == 1:
-        if el not in string.ascii_uppercase:
-            print('Упс! Неизвестная буква. Попробуйте другую!')
-            return False
-        if el in LATIN_GLAS:
-            print(f"{el} - гласная буква!")
-        if el in LATIN_UPPER_LETTERS - (LATIN_UPPER_LETTERS & LATIN_GLAS):
-            print(f"{el} - согласная буква!")
-        return True
-    if num == 2:
-        if el not in CYRILLIC_UPPER_LETTERS:
-            print(f"Упс! Неизвестная буква. Попробуйте другую!")
-            return False
-        if el in CYRILIC_GLAS:
-            print(f"{el} - гласная буква!")
-        if el in CYRILLIC_UPPER_LETTERS - (CYRILLIC_UPPER_LETTERS & CYRILIC_GLAS):
-            print(f"{el} - согласная буква!")
-        return True
+
+def check_letter(
+        vowels: str,
+        char: str,
+        first_and_last_letter: tuple[str, str]
+) -> str:
+    """
+
+    :param vowels:
+    :param char:
+    :param first_and_last_letter: Первая и последняя буква алфавита.
+    :return:
+    """
+    first, last = first_and_last_letter
+
+    if char in vowels:
+        return f'{char} - гласная буква!'
+    elif first < char < last:
+        return f"{char} - согласная буква!"
+
+    return 'Упс! Неизвестная буква. Попробуйте другую!'
 
 
 def main(num, el):
-    if not alphabet_n_letter_of_alphabet(num, el):
-        return None
-    
+    if num == 1:
+        print(check_letter(ENG_VOWELS, el, ('A', 'Z')))
+    elif num == 2:
+        print(check_letter(CYRILLIC_VOWELS, el, ('А', 'Я')))
+
 
 if __name__ == '__main__':
-    
+
     try:
         a = int(input('Введите номер алфавита: '))
-    except ValueError or a not in [1,2]:
+    except ValueError or a not in [1, 2]:
         print("Упс! Выбран неверный режим. Попробуйте ещё раз...")
     else:
         if is_correct_alphabet_number(a):
@@ -57,5 +56,3 @@ if __name__ == '__main__':
                 main(a, input('Введите букву латинского алфавита: ').upper())
             if a == 2:
                 main(a, input('Введите букву кириллицы: ').upper())
-    
-    
